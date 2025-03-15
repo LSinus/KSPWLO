4d756382e5f530efddfe9ae01b25c8e101c7867f
saevuss <giosavo4@gmail.com>
2025-03-15 12:59:54 +0100

    addition of QSocketNotifier

--- frontend/client.py
+++ frontend/client.py
@@ -15,6 +15,7 @@ from geopy.geocoders import Nominatim
 from geopy.geocoders import Nominatim
 from pprint import pprint
 from geopy.distance import geodesic
+from PyQt5.QtCore import QTimer
 
 
 
@@ -123,11 +124,11 @@ class AppIntegrata(QMainWindow):
         min_lon = min(start.longitude, end.longitude) - margin
         max_lon = max(start.longitude, end.longitude) + margin
 
-        G = ox.graph_from_bbox(max_lat, min_lat, max_lon, min_lon, network_type='drive')
+        self.G = ox.graph_from_bbox(max_lat, min_lat, max_lon, min_lon, network_type='drive')
         print("Grafo ricevuto, salvo il file...")
         filepath='files/G.graphml'
-        #G=ox.load_graphml(filepath)
-        ox.save_graphml(G, filepath)
+        #self.G=ox.load_graphml(filepath)
+        ox.save_graphml(self.G, filepath)
 
         from graph_utils import add_osmid, calc_min_dist_osmid
         add_osmid(filepath, filepath)
@@ -142,8 +143,6 @@ class AppIntegrata(QMainWindow):
             graph_data = f.read()
         data = source_dest_bytes + graph_data
 
-
-
         send_data(self.client_socket, data, graph_size)
 
         import bisect 
@@ -151,55 +150,79 @@ class AppIntegrata(QMainWindow):
         distance_km=geodesic((start.latitude, start.longitude), (end.latitude, end.longitude)).km
         print(distance_km)
         distance_limit=[0,5, 1, 5, 20, 50, 150, 200, 300, 400, 500, 600, 700]
-        zoom_levels=[18, 16, 15, 13, 11, 10, 9, 8, 7, 6, 5, 4, 3]
+        zoom_levels=[17, 15, 14, 12, 10, 9, 8, 7, 6, 5, 4, 3, 2]
         index = bisect.bisect_left(distance_limit, distance_km) #bisect executes a binary search of distance_km getting the index
         # Return the corresponding zoom level
         zoom_level=zoom_levels[min(index, len(zoom_levels) - 1)]
-        m=folium.Map(location=[(start.latitude+end.latitude)/2, (start.longitude+end.longitude)/2], zoom_start=zoom_level)
+        
+        
+        self.m=folium.Map(location=[(start.latitude+end.latitude)/2, (start.longitude+end.longitude)/2], zoom_start=zoom_level)
         #adding marker for source and destination
         folium.Marker(location=[start.latitude, start.longitude], popup= "PARTENZA", 
-            icon=folium.Icon(color="green")).add_to(m)
+            icon=folium.Icon(color="green")).add_to(self.m)
         folium.Marker(location=[end.latitude, end.longitude], popup= "ARRIVO", 
-            icon=folium.Icon(color="green")).add_to(m)
+            icon=folium.Icon(color="green")).add_to(self.m)
         from utils import rgb_to_hex
-        folium.PolyLine(locations=((min_lat, min_lon), (max_lat, min_lon), (max_lat, max_lon), (min_lat, max_lon), (min_lat, min_lon)), color=rgb_to_hex(0, 0, 0), weight=6, opacity=1).add_to(m)
+        folium.PolyLine(locations=((min_lat, min_lon), (max_lat, min_lon), (max_lat, max_lon), (min_lat, max_lon), (min_lat, min_lon)), color=rgb_to_hex(0, 0, 0), weight=6, opacity=1).add_to(self.m)
         
-        results=receive_data(self.client_socket)
-        while(results!=None):
+        self.update_map()
+        #when the socket is ready to be read, the signal activated is given, so it is 
+        # linked to this signal the receive_results() function
+        #fileno() methods for socket returns the id of the socket we are using, so we are 
+        # asking QSocketNotifier to track the activiti of the socket of our connection
+        from PyQt5.QtCore import QSocketNotifier
+        self.notifier=QSocketNotifier(self.client_socket.fileno(), QSocketNotifier.Read)
+        self.notifier.activated.connect(self.receive_results)
+        
+        #self.result_timer=QTimer() #timer creation
+        #self.result_timer.timeout.connect(self.receive_results)
+        #self.result_timer.start(100) #check every 100ms
+
+
+    def update_map(self):
+        data=io.BytesIO()
+        self.m.save(data, close_file=False)
+        # adding the QWebEngineView to the existing layout
+        self.webview.setHtml(data.getvalue().decode())
+
+    def receive_results(self):
+        from network_utils import receive_data
+        from utils import rgb_to_hex
+        try: 
+            results=receive_data(self.client_socket)
+            if(results is None):
+                print("fine computazione")
+                #self.result_timer.stop()
+                return
             #print(results)
             for result in results:
                 #onepass+ esx penalty
                 if result.alg_name=="onepass_plus":
-                    route_coords=[(G.nodes[node]['y'], G.nodes[node]['x']) for node in result.list_osmid]
-                    folium.PolyLine(locations=route_coords, color=rgb_to_hex(255, (20*result.num_result)%255, 0), weight=6, opacity=1).add_to(m)
+                    route_coords=[(self.G.nodes[node]['y'], self.G.nodes[node]['x']) for node in result.list_osmid]
+                    folium.PolyLine(locations=route_coords, color=rgb_to_hex(255, (20*result.num_result)%255, 0), weight=6, opacity=1).add_to(self.m)
                     folium.Marker(
                         location=route_coords[len(route_coords)//2],
                         popup=f"onePass risultato n° {result.num_result+1}",
-                        icon=folium.Icon(color="red"), icon_size=(40, 40)).add_to(m)
+                        icon=folium.Icon(color="red"), icon_size=(40, 40)).add_to(self.m)
 
                 elif result.alg_name=="esx":
-                    route_coords=[(G.nodes[node]['y'], G.nodes[node]['x']) for node in result.list_osmid]
-                    folium.PolyLine(locations=route_coords, color=rgb_to_hex((20*result.num_result)%255, 0, 255-10*result.num_result), weight=4, opacity=1).add_to(m)
+                    route_coords=[(self.G.nodes[node]['y'], self.G.nodes[node]['x']) for node in result.list_osmid]
+                    folium.PolyLine(locations=route_coords, color=rgb_to_hex((20*result.num_result)%255, 0, 255-10*result.num_result), weight=4, opacity=1).add_to(self.m)
                     folium.Marker(
                         location=route_coords[len(route_coords)//2],
                         popup=f"esx risultato n° {result.num_result+1}",
-                        icon=folium.Icon(color="blue"), icon_size=(40, 40)).add_to(m)
+                        icon=folium.Icon(color="blue"), icon_size=(40, 40)).add_to(self.m)
 
                 elif result.alg_name=="penalty":
-                    route_coords=[(G.nodes[node]['y'], G.nodes[node]['x']) for node in result.list_osmid]
-                    folium.PolyLine(locations=route_coords, color=rgb_to_hex(0, 255, (20*result.num_result)%255), weight=2, opacity=1).add_to(m)
+                    route_coords=[(self.G.nodes[node]['y'], self.G.nodes[node]['x']) for node in result.list_osmid]
+                    folium.PolyLine(locations=route_coords, color=rgb_to_hex(0, 255, (20*result.num_result)%255), weight=8, opacity=1).add_to(self.m)
                     folium.Marker(
                         location=route_coords[len(route_coords)//2],
                         popup=f"penalty risultato n° {result.num_result+1}",
-                        icon=folium.Icon(color="green"), icon_size=(40, 40)).add_to(m)
-
-            #save map data to data object
-            data=io.BytesIO()
-            m.save(data, close_file=False)
-            # adding the QWebEngineView to the existing layout
-            self.webview.setHtml(data.getvalue().decode())
-            results=receive_data(self.client_socket)
-
+                        icon=folium.Icon(color="green"), icon_size=(40, 40)).add_to(self.m)
+            self.update_map()
+        except BlockingIOError:
+            pass
 
 if __name__ == "__main__":
     parser=argparse.ArgumentParser()
