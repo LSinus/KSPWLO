7d2b8bdcd72a6043b635c8cc0af35b97f034a48b
Lsinus <leonardosinibaldi21@gmail.com>
2025-03-23 18:55:57 +0100

    city radius

--- frontend/utils.py
+++ frontend/utils.py
@@ -1,5 +1,32 @@
+
+
 def rgb_to_hex(r, g, b):
     return '#{:02x}{:02x}{:02x}'.format(r, g, b)
+
+
+def get_city_radìus(city_name):
+    import geonamescache
+
+    gc = geonamescache.GeonamesCache()
+
+    cities = gc.search_cities(city_name, case_sensitive=False, contains_search=False)
+    population = 0
+    for city in cities:
+        if city['population'] > population:
+            population = city['population']
+    radius = 2000
+    if population > 2000000:
+        radius = 15000
+    elif population > 1000000:
+        radius = 10000
+    elif population > 500000:
+        radius = 7000
+    elif population > 150000:
+        radius = 5000
+    elif population > 25000:
+        radius = 4000
+
+    return radius
 
 
 if __name__ == '__main__':
